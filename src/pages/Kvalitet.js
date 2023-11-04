c3fdb590802d5292dfd347850cc673af03e764c6
Josef Malmström <josef.malmstrom@gmail.com>
2023-11-04 21:30:27 +0100

    Finish Kvalitetssäkring

--- src/pages/Kvalitet.js
+++ src/pages/Kvalitet.js
@@ -1,9 +1,40 @@
+import knoppar from "../assets/images/knoppar.jpg";
 import "./Kvalitet.css";
 
 const Kvalitet = () => {
   return (
     <div className="kvalitet-container">
-      <body>Det här är Kvalitetssäkring-sidan.</body>
+      <div className="kvalitet-text">
+        <h1>Kvalitetssäkring</h1>
+        <ul>
+          <li>
+            Medlem i Psykoterapicentrum, riksförening för legitimerade
+            psykoterapeuter.
+          </li>
+          <li>
+            Följer etiska riktlinjer för psykoterapeuter framtagna av
+            Psykoterapicentrum.
+          </li>
+          <li>HBTQ-certifierad.</li>
+          <li>
+            Patientförsäkring och ansvarsförsäkring genom Akademikerförbundet
+            SSR.
+          </li>
+          <li>
+            Följer Socialstyrelsens riktlinjer om journalföring och
+            kvalitetssäkring.
+          </li>
+          <li>
+            Följer etiska riktlinjer för psykoterapihandledare framtagna av
+            Psykoterapicentrum.
+          </li>
+          <li>
+            Använder Ahums administrativa plattform för säker bokning, betalning
+            och videosamtal.
+          </li>
+        </ul>
+      </div>
+      <img className="kvalitet-image" src={knoppar} alt="Växtknoppar"></img>
     </div>
   );
 };
